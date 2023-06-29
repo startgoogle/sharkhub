6b54838d969a5ae58007bca8b80f696110be207c
unblocked6969 <60207373+startgoogle@users.noreply.github.com>
2023-06-29 10:37:12 -0700

    Create kekw.lua

---
+++ kekw.lua
@@ -0,0 +1,14 @@
+workspace.FallenPartsDestroyHeight = -50000
+local placeId = game.PlaceId
+
+if placeId == 6152116144 or placeId == 13883279773 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NovazUwU/SharkHub/main/Map1%22))()
+elseif placeId == 11468159863 or placeId == 13881804983 or placeId == 13883059853 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NovazUwU/SharkHub/main/Map2%22))()
+elseif placeId == 5956785391 then 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NovazUwU/SharkHub/main/Menu%22))()
+elseif placeId == 11468034852 then -- MUGEN
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NovazUwU/SharkHub/main/Mugen%22))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NovazUwU/SharkHub/main/Ouw%22))()
+end
